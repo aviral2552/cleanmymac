27e5175e99b942cf81f9785bb17929b597fa9d19
Lame Hacker <{ID}+{username}@users.noreply.github.com>
2018-11-07 02:49:36 +0530

    anaconda updater/cleaner

---
+++ cleaners/03_anaconda.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Check if Anaconda is installed and upgrade packages
+if hash conda 2>/dev/null; then
+  echo "Updating your Anaconda packages..."
+  conda update --all
+  echo ""
+
+
+### Keeping cleanup disabled as the command above downloads and updates the all packages
+### and cleanup will remove the unused packages, thus uselessly downloading packages.
+
+# Invoked with "cleanmymac cleanup" command
+#  if [[ $1 == "cleanup" ]]; then
+#    echo "Cleaning unused Ananconda packages..."
+#    conda clean -a -y
+#  fi
+#fi
+
+
