f28af099298267485017a8eff836bd732d34e388
Lame Hacker <{ID}+{username}@users.noreply.github.com>
2018-11-07 01:22:34 +0530

    Updated install system

--- cleanmymac.sh
+++ cleanmymac.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Initiating the cleaning engines..."
-echo ""
-
-MAX_WIDTH=50
-CURRENT_WIDTH=$(tput cols)
-COLUMNS=$(( CURRENT_WIDTH > MAX_WIDTH ? MAX_WIDTH : CURRENT_WIDTH ))
-printf '%*s\n' "${COLUMNS}" '' | tr ' ' =
+echo "=================================="
 
 CLEANERS_DIR=$(cat ~/.cleanmymac/path)/cleaners
 
 for script in $(ls $CLEANERS_DIR); do
   if [ -x "$CLEANERS_DIR/$script" ]; then
-    $CLEANERS_DIR/$script $@
+    ( $CLEANERS_DIR/$script $@ )
   fi
 done
 
--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 
 CMM_PATH=`pwd`
 
-# Adding Clean My macOS in PATH for easy terminal access
-ln -fs "${CMM_PATH}"/cleanmymac.sh /usr/local/bin/cleanmymac
-
 # Copying Clean My macOS to home directory
 mkdir -p ~/.cleanmymac && echo "${CMM_PATH}" > ~/.cleanmymac/path
-cp -R "${CMM_PATH}/cleaners" ~/.cleanmymac
+cp -R "${CMM_PATH}/" ~/.cleanmymac
 
-echo "Clean My macOS has been installed. Run Clean My macOS command by typing cleanmymac in your terminal window!"
+# Adding Clean My macOS in PATH for easy terminal access
+ln -fs ~/.cleanmymac/cleanmymac.sh /usr/local/bin/cleanmymac
+
+echo ""
+echo "Clean My macOS has been installed."
+echo "Run Clean My macOS command by typing cleanmymac in your terminal window!"
+echo ""
