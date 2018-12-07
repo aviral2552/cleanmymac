eb2384b7cfedf7f3400e2dc148f280fe96c94876
Lame Hacker <{ID}+{username}@users.noreply.github.com>
2018-12-07 18:31:38 +0530

    Changing script execution behavior based on arguments passed

--- cleanmymac.sh
+++ cleanmymac.sh
@@ -2,11 +2,17 @@
 
 echo "Initiating the cleaning engines..."
 echo "=================================="
+echo ""
 
 CLEANERS=$(cat ~/.cleanmymac/path)/cleaners
+METAMODULE=$(cat ~/.cleanmymac/path)/cleaners/00_meta.sh
 
+if [ -z "$1" ]; then
     for file in $(ls $CLEANERS); do
-        echo "Running cleaner: " $file && $CLEANERS/$file $@
+      echo "Running cleaner: " $file && $CLEANERS/$file
     done
+else
+    $METAMODULE $@
+fi
 
 echo "All done! Your mac is now squeaky clean!"
