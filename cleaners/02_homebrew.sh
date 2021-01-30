7637bfb113972ae9f4462af9d12ac721bf8a2e9e
Aviral <42315362+thelamehacker@users.noreply.github.com>
2021-01-31 02:58:20 +0530

    Cask not installed
    Cask not installed error fix

--- cleaners/02_homebrew.sh
+++ cleaners/02_homebrew.sh
@@ -9,8 +9,8 @@ if hash brew 2>/dev/null; then
   echo "Checking for updates for homebrew..."
   brew update
   brew upgrade
-  brew upgrade -- cask
-  brew outdated --cask | cut -f 1 | xargs brew cask reinstall
+  brew upgrade --cask
+  brew outdated --cask | cut -f 1 | xargs brew reinstall --cask
   echo ""
 
   echo "Calling the brewery doctor for the mandatory health checkup..."
